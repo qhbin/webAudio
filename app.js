4b8b638937089d25c206d3a14f0eb35c5ece1e80
qhbin <925078922@qq.com>
2017-11-26 17:50:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error('Not Found 404');
     err.status = 404;
     next(err);
 });
